9a1f8196d3e638031f4231cacca73ba79c53b438
hexoul <crosien@gmail.com>
2023-08-03 19:56:43 +0900

    Test query

--- src/App.js
+++ src/App.js
@@ -1,16 +1,47 @@
+import { gql, ApolloClient, InMemoryCache } from "@apollo/client";
 import { useCallback, useEffect, useMemo, useRef } from "react";
 import { useCookies } from "react-cookie";
 import * as Phaser from "phaser";
 
 import { Game } from "./scene/Game";
 
+const client = new ApolloClient({
+  uri: "https://gateway.kinolights.com/",
+  cache: new InMemoryCache(),
+});
+
+const query = gql`
+  query QueryAllReviews(
+    $limit: Int!
+    $orderBy: AllReviewsOrderType!
+    $orderOption: OrderOptionType!
+  ) {
+    allReviews(limit: $limit, orderBy: $orderBy, orderOption: $orderOption) {
+      id
+      createdAt
+      movieId
+      userId
+      reviewTitle
+    }
+  }
+`;
+
 const App = () => {
   const [cookies] = useCookies(["KL_AES"]);
   const game = useRef(null);
 
-  const onGameOver = useCallback((score) => {
-    console.log(`ok ${score} ${cookies.KL_AES}`);
-  }, [cookies.KL_AES]);
+  const onGameOver = useCallback(
+    (score) => {
+      client
+        .query({
+          query,
+          variables: { limit: 2, orderBy: "CREATED_AT", orderOption: "ASC" },
+        })
+        .then((result) => console.log(result));
+      console.log(`ok ${score} ${cookies.KL_AES}`);
+    },
+    [cookies.KL_AES]
+  );
 
   const fps = 30;
   const phaserConfig = useMemo(
